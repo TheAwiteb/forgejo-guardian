70fea1bf7d191e0e4f5f10b3edb6456ef40c85bf
Awiteb <a@4rs.nl>
2025-01-01 09:58:29 +0000

    chore: Update files header
    Signed-off-by: Awiteb <a@4rs.nl>

--- src/error.rs
+++ src/error.rs
@@ -1,18 +1,5 @@
-// Simple Forgejo instance guardian, banning users and alerting admins based on
-// certain regular expressions. Copyright (C) 2024 Awiteb <a@4rs.nl>
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published
-// by the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program. If not, see <https://gnu.org/licenses/agpl.txt>.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (C) 2024-2025 Awiteb <a@4rs.nl>
 
 use reqwest::StatusCode;
 
